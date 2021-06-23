3e9f46d3b81fb8aca48a7168d1f2f59a25361f1d
kalteswasser <info.kalteswasser@gmail.com>
2021-06-23 14:44:38 +0200

    0

---
+++ hopin.com_koreba2868_awinceo.com_p0dli0h7mr/config.js
@@ -0,0 +1,8 @@
+
+window._mtm = window._mtm || {};
+window._mtm.projectId = "prj:e9a1d4fe-28aa-4d70-acec-4dcacf7fabde";
+window._mtm.configAutoblocking =
+{
+    "rules": [],
+    "autoblockingEnabled": true
+}
